55b8b6dc5ffffba4929159c74c3ce827ff0eb0d2
bytespeed <68957067+bytespeed@users.noreply.github.com>
2021-12-29 11:37:36 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ByteSpeed CLaSS'
+copyright = '2021, ByteSpeed'
+author = 'ByteSpeed'
 
 release = '0.1'
 version = '0.1.0'
